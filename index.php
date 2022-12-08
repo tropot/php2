978e77bc7d2f98f67712c2fe10f0f6a82c8023ca
tropot <mangustar123@gmail.com>
2022-12-08 22:42:34 +0300

    Am facut server sidu lu php sa lucreze cu client side

--- index.php
+++ index.php
@@ -1,40 +1,116 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title> loplo</title>
-    </head>
-    	
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <body>
-      
-        <form action="" method="POST" id="sendData">
-            <p><input type="number" name="nr" value="" id="nr"></p>
-            <p><input type="submit" name="submit" value="Submit"></p>
-        </form> 
-        
-       
-    </body>
-    <script>
-    $("#sendData").submit(function(e) {
-        //e.preventDefault();
-        var _data = document.getElementById("nr").value;
-        $.ajax({
-                type: "POST",
-                url: "http://localhost/php2/",
-                data: {nr : _data},
-            });
+«<!DOCTYPE html>»
+<html>  
+<body>  
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script> 
+
+<h1><%=buba%></h1>
+
+<form method = "post" id = "sendData">
+  <label for="sendData">Add number to the database : </label><br>
+    <input type="text" id="fname" value="" ><br><br>
+    <input type="submit" value="Submit" >
+  </form> 
+
+  <form method = "post" id = "editData" style="visibility: hidden">
+    <label for="editData">Edit the selected number : </label><br>
+    <input type="text" id="ename" value="" ><br><br>
+    <input type="submit" value="Submit" >
+  </form> 
+
+  <table border="1" class="table table-striped table-bordered" id="sData">
+    <thead>
+        <tr>
+            <th>Nr introdus</th>
+            <th>Nr random</th>
+            <th>Suma</th>
+            <th>Id</th>
+            <th>edit</th>
+        </tr>
+    </thead>
+    <tbody></tbody>
+</table>
+
+  <script>
+    var selectedId;
+    var oldNum;
+    $(document).ready(function(){
+      load_data();
+      $("#sendData").submit(function(e) {
+        e.preventDefault();
+        var inputNr = document.getElementById('fname').value;
+        add_data(inputNr);
+        load_data();
+        //document.getElementById("sendData").style.display = "none";
     });
-</script>
-<?php
-    if( isset($_POST['nr']) ){
-        echo $_POST['nr'];
-        error_log($_POST['nr']);
-        exit;
-      }
+    $("#editData").submit(function(e) {
+        e.preventDefault();
+        var inputNr = document.getElementById('ename').value;
+        edit_data(inputNr,selectedId,oldNum);
+        load_data();
+    });
     
-?>
+    
+    function add_data(number)
+    {
+      $.post('script.php', { action: 'add' , newData: number}, function(data) {
+        console.log(data);
+        load_data();
+      });
+      
+    }
+    function load_data()
+    {
+      $.post('script.php', { action: 'fetch' }, function(data) {
+        var data = JSON.parse(data);
+        var html = '';
+        
+                if(data.length > 0)
+                {
+                    for(var count = 0; count < data.length; count++)
+                    {
+                      var url = '<a href="http://localhost:3000/edit?inputNumber='+data[count].input+'&uid='+ data[count].id +'">edit</a>';  
+                      html += `
+                        <tr>
+                            <td>`+data[count].input_number+`</td>
+                            <td>`+data[count].random_number+`</td>
+                            <td>`+data[count].number_result+`</td>
+                            <td>`+data[count].id+`</td>
+                            
+                            <td><button type="button" id='`+data[count].id+`' onclick='showForm(`+data[count].id+`,`+data[count].input_number+`)' >edit</button></td>
+                            
+                        </tr>
+                        
+                        `;
+                    }
+                }
 
-</html> 
+                $('#sData tbody').html(html);
+      });
+      
+    }
 
-//C:\xampp\php\php -S localhost:8085 consola la php
+    });
+    function edit_data(number,tId,oldInput)
+        {
+          document.getElementById("editData").style.visibility = "hidden";
+            $.post('script.php', { action: 'edit' , newData: number,id : tId,old : oldInput}, function(data) {
+              
+            console.log(data);
+            load_data();
+          });
+          
+        }
+    function showForm(id,number){
+      document.getElementById("editData").style.visibility = "visible";
+      document.getElementById("ename").value = number;
+      selectedId = id;
+      oldNum = number;
+      console.log(id, "   ", number);
+    }
+
+    
+  </script>
+  <p id = "p1" style="border-width:3px; border-style:solid; border-color:#FF0000; padding: 1em;">void</p>
+</body>
+</html>
+
--- script.php
+++ script.php
@@ -1,6 +1,54 @@
 <?php
-//error_log("post : ");
-error_log($_POST['nr']);
-//var_dump($_POST);
-//echo "response";
+ include 'db.php';
+ $conn = new mysqli($servername,$username,$password,$dbname);
+ $nr_random = random_int(1, 25);
+ if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+ if (isset($_POST['action'])) {
+    switch ($_POST['action']) {
+            case 'fetch':
+                $result = mysqli_query($conn,"SELECT * FROM numeros");
+                while($row = mysqli_fetch_assoc($result)){
+                    $arr[] = $row;
+                }
+                echo json_encode($arr);
+                exit;
+                break;
+            case 'add':
+                $numar = (int)$_POST['newData'];
+                $suma = $numar + $nr_random;
+                $sql = "INSERT INTO numeros (input_number, random_number, number_result)
+                VALUES ($numar, $nr_random, $suma)";
+                if ($conn->query($sql) === TRUE) {
+                    echo "numbers are posted to the database";
+                    } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+                exit;
+                break;
+            case 'edit':
+
+                $nD = $_POST['newData'];
+                $iD = $_POST['id'];
+                $sql = "UPDATE numeros SET input_number= '$nD' WHERE id='$iD'";
+                if ($conn->query($sql) === TRUE) {
+                    echo "numbers are edited";
+                    } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+
+                exit;
+                break;
+        }
+     
+    
+     
+
+     
+      
+    
+
+ }
+ 
 ?>
